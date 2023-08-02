bf4deb85dec41ad893d29dfc0d5adb37f512ce28
Fuzus <matheusdamata2000@gmail.com>
2023-08-02 20:09:37 -0300

    first commit

---
+++ src/main/java/br/com/itau/casebackendjr/CaseBackendJrApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.casebackendjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseBackendJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseBackendJrApplication.class, args);
+	}
+
+}
