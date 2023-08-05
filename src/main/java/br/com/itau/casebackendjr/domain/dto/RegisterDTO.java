64c1c7c75e2f47507ebf7f27e17ce7597139d2a4
Fuzus <matheusdamata2000@gmail.com>
2023-08-05 16:36:33 -0300

    Adicionando json alias para os campos de input da API

--- src/main/java/br/com/itau/casebackendjr/domain/dto/RegisterDTO.java
+++ src/main/java/br/com/itau/casebackendjr/domain/dto/RegisterDTO.java
@@ -1,21 +1,26 @@
 package br.com.itau.casebackendjr.domain.dto;
 
 import br.com.itau.casebackendjr.domain.model.Register;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RegisterDTO(
         Long id,
         @NotBlank
+        @JsonAlias({"nome", "firstName"})
         String firstName,
         @NotBlank
+        @JsonAlias({"sobrenome", "lastName"})
         String lastName,
         @NotNull
+        @JsonAlias({"idade", "age"})
         Integer age,
         @NotBlank
+        @JsonAlias({"pais", "country"})
         String country
 ) {
-    public RegisterDTO(Register data){
+    public RegisterDTO(Register data) {
         this(data.getId(), data.getFirstName(), data.getLastName(), data.getAge(), data.getCountry());
     }
 }
--- src/main/java/br/com/itau/casebackendjr/domain/dto/RegisterUpdateDTO.java
+++ src/main/java/br/com/itau/casebackendjr/domain/dto/RegisterUpdateDTO.java
@@ -1,9 +1,15 @@
 package br.com.itau.casebackendjr.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public record RegisterUpdateDTO(
+        @JsonAlias({"nome", "firstName"})
         String firstName,
+        @JsonAlias({"sobrenome", "lastName"})
         String lastName,
+        @JsonAlias({"idade", "age"})
         Integer age,
+        @JsonAlias({"pais", "country"})
         String country
 ) {
 }
